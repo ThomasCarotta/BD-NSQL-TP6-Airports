e919a0457cac430a3e2039926e8315381953889c
ThomasCarotta <thomasgabrielcarotta@outlook.com>
2025-06-12 20:28:49 +0000

    BD NSQL - TP6

--- airports/urls.py
+++ airports/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("airports", views.create_airport),              # POST
-    path("airports", views.list_airports),               # GET all
-    path("airports/popular", views.get_popular_airports),
-    path("airports/nearby", views.get_nearby_airports),
-    path("airports/<str:iata>", views.get_airport),      # GET one
-    path("airports/<str:iata>", views.update_airport),   # PUT
-    path("airports/<str:iata>", views.delete_airport),   # DELETE
+    path('airports', views.list_airports),
+    path('airports/<str:iata>', views.get_airport),
+    path('airports/<str:iata>', views.update_airport),
+    path('airports/<str:iata>', views.delete_airport),
+    path('airports/nearby/', views.get_nearby_airports),
+    path('airports/popular', views.get_popular_airports),
+    path('airports/', views.create_airport),  # Para POST
 ]
