a76e85213e08b2a70e72a9bc4ff616a89494c2b8
ThomasCarotta <thomasgabrielcarotta@outlook.com>
2025-06-13 19:49:49 +0000

    TP6 - BD NSQL

--- airports/urls.py
+++ airports/urls.py
@@ -1,16 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('airports', views.list_airports),
-#     path('airports/<str:iata>', views.get_airport),
-#     path('airports/<str:iata>', views.update_airport),
-#     path('airports/<str:iata>', views.delete_airport),
-#     path('airports/nearby/', views.get_nearby_airports),
-#     path('airports/popular', views.get_popular_airports),
-#     path('airports/', views.create_airport),  # Para POST
-# ]
-
 from django.urls import path
 from . import views
 
